d7a71828a35b52551a132d6d1f70271f5b84e99a
Romina <Romina.kehl@googlemail.com>
2019-03-08 21:34:31 +0100

    View angelegt

--- src/main/java/de/eit7/rvwbk/rominakehl/myfirstapp2019/MainActivity.java
+++ src/main/java/de/eit7/rvwbk/rominakehl/myfirstapp2019/MainActivity.java
@@ -2,12 +2,38 @@ package de.eit7.rvwbk.rominakehl.myfirstapp2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TextViews
+    private TextView gameHint = null;
+    private TextView gameResult = null;
+
+    //EditTexts
+    private EditText numberInput = null;
+
+    //Buttons
+    private Button btnSubmit = null;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.gameHint = (TextView) findViewById(R.id.txtHintGame);
+        this.gameResult = (TextView) findViewById(R.id.txtResult);
+        this.numberInput = (EditText) findViewById(R.id.txtNumberInput);
+        this.btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
+
+
+
     }
 }
